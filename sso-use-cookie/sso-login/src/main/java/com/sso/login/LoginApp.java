5e61417be3b865201457d15104173c0838058022
lyx <1831245059@qq.com>
2021-10-21 14:40:25 +0800

    Changes

--- sso-use-cookie/sso-login/src/main/java/com/sso/login/LoginApp.java
+++ sso-use-cookie/sso-login/src/main/java/com/sso/login/LoginApp.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoginApp {
+    // 发顺丰
+    // 所产生的
     public static void main(String[] args) {
         SpringApplication.run(LoginApp.class,args);
     }
